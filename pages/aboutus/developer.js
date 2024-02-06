f26cd0cc2bf161c482ea6940bb4bebe5eb613dda
nihar078 <nihar078@gmail.com>
2024-02-06 20:03:48 +0530

    task_2_Making Nested Routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,12 @@
+// our-domain.com/aboutus/developer
+
+const DeveloperPage = () => {
+  return (
+    <div>
+      <h1>Developer</h1>
+      <h6>My Name is Nihar Ranjan Biswal</h6>
+    </div>
+  );
+};
+
+export default DeveloperPage;
